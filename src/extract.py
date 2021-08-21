331ae37f98bf6bce8535106c6102d90b7f81d6da
RLP2 <75913524+RLP2@users.noreply.github.com>
2021-08-21 14:41:32 -0500

    Update extract.py

--- src/extract.py
+++ src/extract.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from dataclasses import dataclass
 
 url = 'https://api.solanabeach.io/v1/'
 
@@ -10,7 +11,8 @@ class BearerAuth(requests.auth.AuthBase):
         r.headers["authorization"] = "Bearer " + self.token
         return r
 
-def _solana_api(limit:int = 1, cursor:str = None, token:str = None):
+# API CALL
+def _solana_api(limit:int = 1, cursor:int = None):
   '''
   API for fetching latest blocks
   
@@ -21,22 +23,18 @@ def _solana_api(limit:int = 1, cursor:str = None, token:str = None):
   Return:
     Response(Json) = returned api json
   '''
-  
   api_get = 'latest-blocks'
   
   if not isinstance(limit, int):
     raise TypeError(f'_solana_api: limit requires integer value')
   
-  if not token:
-    raise TypeError(f'_solana_api requires api token')
-  
-  if cursor and not isinstance(cursor, str):
-    raise TypeError(f'_solana_api: cursor requires str value')
+  if cursor and not isinstance(cursor, int):
+    raise TypeError(f'_solana_api: cursor requires integer value')
 
   # Set Query Parameters
   query = {'limit': limit}
   if cursor:
-    query['cursor'] = cursor
+    query['cursor'] = str(cursor)
   
   # Call API
   response = requests.get(url + api_get, params=query, auth=BearerAuth(token))
@@ -45,3 +43,77 @@ def _solana_api(limit:int = 1, cursor:str = None, token:str = None):
     raise Error(f"_solana_api api failed: {response}")
 
   return response.json()
+
+# TRANSFORM
+@dataclass
+class SolBlock(object):
+  '''
+  Class for solana block data
+  '''
+  def __init__(self):
+    self.blocknumber: int
+    self.blockhash: str
+    self.previousblockhash: str
+    self.parentslot: int
+    self.blocktime_abs: float
+    self.blocktime_rel: float
+    self.txcount: int
+    self.failedtxs: int
+    self.totalfees : float
+    self.instructions: int
+    self.sucessfultxs: int
+    self.totalvaluemoved: float
+    self.innerinstructions: int
+
+def transform_json(response:json, start_number:float=-float('inf')):
+  '''
+  Function to parse api response for block data
+  
+  Args:
+    response(json): successful response from api
+
+  Retrun:
+    chain(list): list of SolBlocks
+  '''
+
+  chain = []
+  start_block_number = float('inf')
+  end_block_number = -float('inf')
+  for record in response:
+    block = SolBlock()
+    block.blocknumber = record['blocknumber']
+    block.blockhash = record['blockhash']
+    block.previousblockhash = record['previousblockhash']
+    block.parentslot = record['parentslot']
+    block.blocktime_abs = record['blocktime']['absolute']
+    block.blocktime_rel = record['blocktime']['relative']
+    block.txcount = record['metrics']['txcount']
+    block.failedtxs = record['metrics']['failedtxs']
+    block.totalfees = record['metrics']['totalfees']
+    block.instructions = record['metrics']['instructions']
+    block.sucessfultxs = record['metrics']['sucessfultxs']
+    block.totalvaluemoved = record['metrics']['totalvaluemoved']
+    block.innerinstructions = record['metrics']['innerinstructions']
+    if block.blocknumber <= start_number:
+      break
+    chain.append(block)
+
+    start_block_number = min(start_block_number, record['blocknumber'])
+    end_block_number = max(end_block_number, record['blocknumber'])
+  return chain, start_block_number, end_block_number
+
+# BLOCKS TO APPEND
+def create_chain(start_number:int):
+  latest_block = _solana_api(limit = 1)
+  end_number = latest_block[0]['blocknumber']
+  init_end_number = end_number
+  chain = []
+  while start_number + 1 < end_number:
+    
+    diff = min(50, end_number - start_number)
+    blocks = _solana_api(limit = diff, cursor=end_number)
+    rollup, start_roll_number, end_roll_number = transform_json(blocks, start_number)
+    chain.extend(rollup)
+    end_number = start_roll_number
+
+  return chain, start_number, end_number
