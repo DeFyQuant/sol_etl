05a49e61f03dcfb5d690be7b1e1b84f69a9e6c4e
RLP2 <75913524+RLP2@users.noreply.github.com>
2021-08-22 14:13:57 -0500

    Update max_block_query.py

--- src/max_block_query.py
+++ src/max_block_query.py
@@ -3,7 +3,16 @@ import logging
 from google.cloud import bigquery
 
 def get_max(table_id:str, dataset_id:str, client:bigquery.Client):
-
+    '''
+    Query to return the latest block in saved data
+    Args:
+        table_id(str): table id of saved data
+        dataset_id(str): dataset id of saved data
+        client(Client): bigQuery client
+    
+    Return:
+        maxblocknumber(int): id of most recent saved block    
+    '''
     dataset = client.create_dataset(dataset_id)
 
     query = f"""
